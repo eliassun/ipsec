0ebc6aac531ee55c4db516f8da9c5cbef6ec0d4a
Elias Sun <eliassun@gmail.com>
2022-12-14 19:55:56 -0800

    update AMI

--- site2/install_softwares.sh
+++ site2/install_softwares.sh
@@ -1,13 +1,16 @@
 #! /bin/bash
 
 cd /home/ubuntu
+mkdir install
+cd install
 sudo apt-get update > update.log
+sudo apt-get update > upgrade.log
 sudo apt install strongswan strongswan-pki libcharon-extra-plugins libcharon-extauth-plugins libstrongswan-extra-plugins libtss2-tcti-tabrmd0 net-tools -y > strong.log
 echo "installing certs..." > certs.log
 sudo ipsec pki --gen --size 4096 --type rsa --outform pem > /etc/ipsec.d/private/ca.key.pem
 sudo ipsec pki --self --in /etc/ipsec.d/private/ca.key.pem --type rsa --dn "CN=site2@elias" --ca --lifetime 3650 --outform pem ca.cert.pem > /etc/ipsec.d/cacerts/ca.cert.pem
 sudo ipsec pki --gen --size 4096 --type rsa --outform pem > /etc/ipsec.d/private/server.key.pem
-sudo IP=`curl http://checkip.amazonaws.com`
+IP=`curl http://checkip.amazonaws.com`
 echo $IP > ip.log
 sudo ipsec pki --pub --in /etc/ipsec.d/private/server.key.pem --type rsa | ipsec pki --issue --lifetime 3650 --cacert /etc/ipsec.d/cacerts/ca.cert.pem --cakey /etc/ipsec.d/private/ca.key.pem --dn "CN=<$IP>" --san="<$IP>" --flag serverAuth --flag ikeIntermediate --outform pem > /etc/ipsec.d/certs/server.cert.pem
 echo "enable forwarding..." > forward.log
